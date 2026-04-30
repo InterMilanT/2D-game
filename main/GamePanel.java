ceef9534c38e45d6798cfca087306e50384c9174
InterMilanT <ST10508318@myemeris.edu.za>
2026-04-30 13:04:49 +0200

    updated main
    Started the program off

--- main/GamePanel.java
+++ main/GamePanel.java
@@ -1,5 +1,11 @@
 package main;
 
-public class GamePanel {
+import javax.swing.JPanel;
 
+public class GamePanel extends JPanel
+{
+	//SCREEN SETTINGS
+	final int originalTileSize = 18 ; // 16x16 tile
+	final int scale = 3 ;
+	
 }
